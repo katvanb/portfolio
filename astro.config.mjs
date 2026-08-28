9c44192e8ccac2f5f350e09c2387bbf2b4e897d6
Katherine Gaspare <Katherine@bertonisolutions.com>
2026-08-28 15:46:49 -0400

    Fix portfolio navigation and language routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://katvanb.github.io',
-  base: '/portfolio'
+  base: '/portfolio',
 });
